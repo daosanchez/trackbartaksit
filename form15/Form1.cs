d1f726e186e7e6756c2139cd6247e230677cc0dd
ravza <145916188+ravzasanchez@users.noreply.github.com>
2023-10-02 16:37:16 +0300

    Add files via upload

---
+++ form15/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int taksit, tutar;
+            double taksitliTutar;
+            taksit = tbTaksit.Value;
+            tutar = Convert.ToInt32(txtTutar.Text);
+
+            taksitliTutar =s(double) tutar / taksit;
+
+            lblSonuc.Text = taksitliTutar.ToString();
+
+            
+        }
+
+        private void tbTaksit_Scroll(object sender, EventArgs e)
+        {
+            lblTaksit.Text = tbTaksit.Value.ToString();
+        }
+    }
+}
